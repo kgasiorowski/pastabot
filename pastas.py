12d557e9ba0dfd3ea10b66f2e5a292ce731afbd1
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2019-09-03 00:17:57 -0400

    (This file was meant to be included in the previous commit)

---
+++ pastas.py
@@ -0,0 +1,21 @@
+import praw
+import config
+
+
+def get_pastas():
+
+    reddit = praw.Reddit(client_id=config.reddit_id,
+                         client_secret=config.reddit_secret,
+                         user_agent='pastabot',
+                         username=config.reddit_user,
+                         password=config.reddit_pw)
+
+    hot_subreddit = reddit.subreddit('copypasta').hot(limit=20)
+
+    pastalist = []
+
+    for post in hot_subreddit:
+        if 0 < len(post.selftext) <= 2000:
+            pastalist.append(post.selftext)
+
+    return pastalist
